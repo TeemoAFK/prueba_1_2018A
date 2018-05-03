c5c639efaf33d052e36af5ed1a5fab207afdc65d
DALISS\Daliss <dayana.rodriguez@epn.edu.ec>
2018-05-02 23:58:55 -0500

    Funciones de las piramides

--- prueba_1_2018A.py
+++ prueba_1_2018A.py
@@ -1,46 +1,47 @@
 import math
-valor = 0
-
+valor=0
+valor1=0
 
 def cubo(lado):
     return lado * lado * lado
 
+def piracuadrada(base, altura):
+    areabase = base**2
+    return (areabase * altura) / 3
 
-def piracuadrada(area,base):
-    return (base*area)/3
-
-def fun2():
-    return 0
-
+def piratriangular(base, altura):
+    areabase=(base*altura)/2
+    return (areabase*altura)/3
 
 def esfera(radio):
     return 3 / 4 * math.pi * radio
-
 
 opcion = 0
 while (opcion != 5):
     print("PRUEBA 1")
     print("Volumen de figuras geometricas")
     print("1. Cubo")
-    print("2. Piramide Cuadrada")
-    print("3. ")
+    print("2. Piramide base cuadrada")
+    print("3. Piramide base triangular")
     print("4. Esfera")
     print("5. Salir")
     opcion = int(input("Seleccione una opcion: "))
 
     if opcion == 1:
-        valor = float(input("Ingrese el lado del cubo"))
-        print("Resultado", cubo(valor))
+        valor = float(input("Ingrese el lado del cubo: "))
+        print("Resultado: ", cubo(valor))
     if (opcion == 2):
-        valor=float(input("Ingrese el area de la base"))
-        valor1=float(input("Ingrese la altura"))
-        piracuadrada(valor,valor1)
+        valor=float(input("Ingrese la base: "))
+        valor1=float(input("Ingrese la altura: "))
+        print("Resultado: ", piracuadrada(valor,valor1))
     if (opcion == 3):
-        func2()
+        valor = float(input("Ingrese la base: "))
+        valor1 = float(input("Ingrese la altura: "))
+        print("Resultado: ", piratriangular(valor, valor1))
     if opcion == 4:
-        valor = float(input("Ingrese el radio de la esfera"))
-        esfera(valor)
+        valor = float(input("Ingrese el radio de la esfera: "))
+        print("Resultado: ",esfera(valor))
     if opcion == 5:
-        print("Salir")
+        print("Salir!")
     if opcion >= 6:
-        print("Seleccione una opcion correcta")
+        print("Seleccione una opcion correcta!")
